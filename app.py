88899731a49e081fc45f74c437ac53a7a905d5cb
Lucas <75747405+Lucas-02@users.noreply.github.com>
2021-10-12 06:47:00 -0700

    Changed file extension to txt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return render_template('index.html')
+   return render_template('index.txt')
 
 @app.route('/', methods=['POST'])
 def form():
@@ -13,7 +13,7 @@ def form():
    url = text
    print(text)
    video = pafy.new(url)
-   return render_template('stats.html', video=video)
+   return render_template('stats.txt', video=video)
 
 if __name__ == "__main__":
    app.run()
